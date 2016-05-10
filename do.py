bcda5ca6e80d142001342b43dd0f0c815781bb95
Dima Blaginin <dm.blaginin@gmail.com>
2016-05-10 14:54:22 +0500

    getting raw data

--- do.py
+++ do.py
@@ -31,8 +31,14 @@ class DevHandler(BaseHandler):
 	def post(self, url):
 		url = url[1:]
 		if url.startswith('git'):
-			print('debugdata', self.get_argument("ref", default=''))
-			if self.request.headers.get('X-Github-Event', '-1') == 'push' and self.get_argument("ref", default='').split('/') == 'master':
+
+			try: 
+				postdata = json.loads(self.request.body.decode(encoding='utf8'))
+			except BaseException:
+				return
+
+			print('debugdata', postdata)
+			if self.request.headers.get('X-Github-Event', '-1') == 'push' and postdata.get("ref", '').split('/') == 'master':
 				sh0 = self.request.headers.get('X-Hub-Signature', '-1').split('=')[-1]
 				print('\n~~~~~~~~~ git updating session ~~~~~~~~~\n',  url,  sh0)
 				sh1 = hmac.new(GIT_SECRET_KEY, msg=self.request.body, digestmod=sha1)
