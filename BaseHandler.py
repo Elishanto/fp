0ed8bb05d943b61f5edae6a07dcce182f5c17773
elishanto <elishanto@gmail.com>
2016-05-10 22:00:17 +0300

    Add config file Fix code style

--- BaseHandler.py
+++ BaseHandler.py
@@ -1,47 +1,49 @@
-import tornado
 import tornado.web
-import tornado.auth
 from pymongo import MongoClient
-client = MongoClient() #базы данных
+
+client = MongoClient()  # базы данных
+
 
 class BaseHandler(tornado.web.RequestHandler):
-	database = client['health']
-	
-	def get_current_user(self):
-		a = self.get_secure_cookie("user")
-		if a is None: return(None)
-		t = int(a.decode('utf8'))
-	
-		if self.check_via_login(t):
-			return(t)
-		else:
-			print('пользователь удалён')
+    def data_received(self, chunk):
+        pass
 
-	def check_via_login(self, login):
-		try:
-			if self.database['users'].find_one({'id':int(login)})['valid'] == 1:
-				return(True)
-		except (TypeError, ValueError) : pass
-		return(False)
+    database = client['health']
 
+    def get_current_user(self):
+        a = self.get_secure_cookie("user")
+        if a is None:
+            return None
+        t = int(a.decode('utf8'))
 
-	def get_user_info(self, uid, task=[]):
-		anser = {}
-		print('\t get', uid, task)
-		if len(task) == 0: 
-			print('\return', self.database['users'].find_one({'id':uid}), '\n')
-			return(self.database['users'].find_one({'id':uid}))
-		
-		print('\t return', self.database['users'].find_one({'id':uid}, dict(zip(task, [1]*len(task)))), '\n')
-		return(self.database['users'].find_one({'id':uid}, dict(zip(task, [1]*len(task)))))
+        if self.check_via_login(t):
+            return t
+        else:
+            print('пользователь удалён')
 
+    def check_via_login(self, login):
+        try:
+            if self.database['users'].find_one({'id': int(login)})['valid'] == 1:
+                return True
+        except (TypeError, ValueError):
+            pass
+        return False
 
-	def push_data(self, indificator, path, task):
-		for i in task:
-			self.database[path].update(indificator, {'$pushAll': task})
+    def get_user_info(self, uid, task=None):
+        if task is None:
+            task = []
+        print('\t get', uid, task)
+        if len(task) == 0:
+            print('\treturn', self.database['users'].find_one({'id': uid}), '\n')
+            return self.database['users'].find_one({'id': uid})
 
+        print('\t return', self.database['users'].find_one({'id': uid}, dict(zip(task, [1] * len(task)))), '\n')
+        return self.database['users'].find_one({'id': uid}, dict(zip(task, [1] * len(task))))
 
-	def set_data(self, indificator, path, task):
-		print(task, path, indificator, task)
-		self.database[path].update(indificator, {'$set': task}, False, True)
+    def push_data(self, identificator, path, task):
+        for _ in task:
+            self.database[path].update(identificator, {'$pushAll': task})
 
+    def set_data(self, identificator, path, task):
+        print(task, path, identificator, task)
+        self.database[path].update(identificator, {'$set': task}, False, True)
--- do.py
+++ do.py
@@ -1,145 +1,137 @@
+import hmac
+import json
+import os
+from hashlib import sha1
+
 import tornado
-import tornado.web
 import tornado.auth
 import tornado.httpserver
-from BaseHandler import BaseHandler
-from baseapi import api, systemfunctions
-import os 
+import tornado.web
 import ui
-import json
-from hashlib import sha1
-import hmac
+from BaseHandler import BaseHandler
+from baseapi import Api, systemfunctions
+import yaml
 
-port = 80 #http доступ
+port = 8000  # http доступ
+config = yaml.load(open('config.yml', 'rb'))
 os.getenv(str(port))
- 
-os.getenv(str(port)) 
 
-GIT_SECRET_KEY = bytes('4297d0f6378e63675607cb6ab5005e503cb26b66', encoding='utf8')
+os.getenv(str(port))
 
-decription = {
-	10: 'отжимания',
-	11: 'потдягивания',
-	54: 'поедание круасанчиков',
-	777: 'жокирство', 
-	778: 'чтение',
-	12 : 'пресс',
-	13 : 'приседания'
-}
- 
+GIT_SECRET_KEY = bytes(config['GIT_SECRET_KEY'], encoding='utf8')
+
+description = config['description']
+
 
 class DevHandler(BaseHandler):
-	def post(self, url):
-		url = url[1:]
-		if url.startswith('git'):
+    def post(self, url):
+        url = url[1:]
+        if url.startswith('git'):
 
-			try: 
-				postdata = json.loads(self.request.body.decode(encoding='utf8'))
-			except BaseException:
-				return
- 
-			if self.request.headers.get('X-Github-Event', '-1') == 'push' and postdata.get("ref", '').split('/')[-1] == 'master':
-				sh0 = self.request.headers.get('X-Hub-Signature', '-1').split('=')[-1]
-				print('\n~~~~~~~~~ git updating session ~~~~~~~~~\n',  url,  sh0)
-				sh1 = hmac.new(GIT_SECRET_KEY, msg=self.request.body, digestmod=sha1)
-				if hmac.compare_digest(sh1.hexdigest(), sh0):
-					print('OK, GITHUB CHECKED')
-					os.system('bash ../updater.sh')
-				else:
-					print('THIS IS NOT GITHUB!')
-			else:
-				print('Bad args,', self.request.headers.get('X-Github-Event', '-1'),  postdata.get("ref", ''))
+            try:
+                postdata = json.loads(self.request.body.decode(encoding='utf8'))
+            except BaseException:
+                return
 
-  
+            if self.request.headers.get('X-Github-Event', '-1') == 'push' \
+                    and postdata.get("ref", '').split('/')[-1] == 'master':
+                sh0 = self.request.headers.get('X-Hub-Signature', '-1').split('=')[-1]
+                print('\n~~~~~~~~~ git updating session ~~~~~~~~~\n', url, sh0)
+                sh1 = hmac.new(GIT_SECRET_KEY, msg=self.request.body, digestmod=sha1)
+                if hmac.compare_digest(sh1.hexdigest(), sh0):
+                    print('OK, GITHUB CHECKED')
+                    os.system('bash ../updater.sh')
+                else:
+                    print('THIS IS NOT GITHUB!')
+            else:
+                print('Bad args,', self.request.headers.get('X-Github-Event', '-1'), postdata.get("ref", ''))
 
- 
+
 class DebugHandler(BaseHandler):
-	def get(self, command):
-		command = command[2:-1]
-		#os.system(command)
-		self.write('calcelled')
-		 
-  
-		
+    def get(self, command):
+        command = command[2:-1]
+        # os.system(command)
+        self.write('calcelled')
+
 
 class LoginHandler(BaseHandler):
-	def get(self, url):
-		if self.request.uri[7:].lower().startswith('exit'):
-			self.clear_cookie("user")
-			self.redirect("/")
+    def get(self, url):
+        if self.request.uri[7:].lower().startswith('exit'):
+            self.clear_cookie("user")
+            self.redirect("/")
 
-		self.render('static/login_activity.html', result=None)
+        self.render('static/login_activity.html', result=None)
 
-	def post(self, url=None):
-		user = self.get_argument("name")
-		try:
-			redirect = int(self.get_argument("redirect"))
-		except BaseException:
-			redirect = 1
-		
-		if self.check_via_login(user):
-			self.set_secure_cookie("user", user)
-			if redirect:
-				self.redirect("/")
-		else:
-			self.render('static/login_activity.html', result='это ложь')
-			
+    def post(self, url=None):
+        user = self.get_argument("name")
+        try:
+            redirect = int(self.get_argument("redirect"))
+        except BaseException:
+            redirect = 1
+
+        if self.check_via_login(user):
+            self.set_secure_cookie("user", user)
+            if redirect:
+                self.redirect("/")
+        else:
+            self.render('static/login_activity.html', result='это ложь')
 
 
 class MainHandler(BaseHandler):
-	@tornado.web.authenticated
-	def get(self):
-	
-		try: self.redirect("/e/"+str(self.get_user_info(self.get_current_user(), ['opened_ex'])['opened_ex'][0]))
-		except IndexError: self.write('У вас нет доступных упражнений')
+    @tornado.web.authenticated
+    def get(self):
+
+        try:
+            self.redirect("/e/" + str(self.get_user_info(self.get_current_user(), ['opened_ex'])['opened_ex'][0]))
+        except IndexError:
+            self.write('У вас нет доступных упражнений')
+
 
 class SMHandler(BaseHandler):
-	@tornado.web.authenticated
-	def get(self):
-		ex = int(self.request.uri[3:].split('/')[0])
-		#oNLY INT
-		opened_ex = self.get_user_info(self.get_current_user(), ['opened_ex'] )['opened_ex']
-		if ex not in opened_ex:
-			self.write('Вам недоступно это упражнение')
-			self.finish()
-		
-		 
-		else:
-			basedata = {'data':self.get_user_info(self.get_current_user(), ['name', 'email', 'wt', 'ht'] ), 'exer_code':ex, 'decription':decription[ex], 'opened_ex':opened_ex, 'decriptions':decription, 'avatar':systemfunctions.get_user_url(self.get_current_user())}
-			basedata['data']['avatar'] = systemfunctions.get_user_url(self.get_current_user())
-			
-			bsdk = set(basedata['data'].keys())
-			print(bsdk, ('name' not in bsdk) , ('email' not in bsdk) , ('wt' not in bsdk), ('ht' not in bsdk))
-			if 'name' not in bsdk or 'email' not in bsdk or 'wt' not in bsdk or 'ht' not in bsdk:
-				self.render('static/ui_setuserdata.html', data=basedata)
-			elif self.database['data.{0}'.format(self.get_current_user())]['stat'].find_one({'ex': ex}) is None:
-				self.render('static/first_run.html', data=basedata)
-			else:
-				self.render('static/main_activity.html', data=basedata)
+    @tornado.web.authenticated
+    def get(self):
+        ex = int(self.request.uri[3:].split('/')[0])
+        # oNLY INT
+        opened_ex = self.get_user_info(self.get_current_user(), ['opened_ex'])['opened_ex']
+        if ex not in opened_ex:
+            self.write('Вам недоступно это упражнение')
+            self.finish()
+        else:
+            basedata = {'data': self.get_user_info(self.get_current_user(), ['name', 'email', 'wt', 'ht']),
+                        'exer_code': ex, 'description': description[ex], 'opened_ex': opened_ex,
+                        'descriptions': description, 'avatar': systemfunctions.get_user_url(self.get_current_user())}
+            basedata['data']['avatar'] = systemfunctions.get_user_url(self.get_current_user())
+
+            bsdk = set(basedata['data'].keys())
+            print(bsdk, ('name' not in bsdk), ('email' not in bsdk), ('wt' not in bsdk), ('ht' not in bsdk))
+            if 'name' not in bsdk or 'email' not in bsdk or 'wt' not in bsdk or 'ht' not in bsdk:
+                self.render('static/ui_setuserdata.html', data=basedata)
+            elif self.database['data.{0}'.format(self.get_current_user())]['stat'].find_one({'ex': ex}) is None:
+                self.render('static/first_run.html', data=basedata)
+            else:
+                self.render('static/main_activity.html', data=basedata)
 
 
 settings = {
-	"cookie_secret": "*****",
-	"login_url": "/login",
-	#"xsrf_cookies": True,
-	'debug' : True,
-	"ui_modules": ui,
-	
+    "cookie_secret": "*****",
+    "login_url": "/login",
+    # "xsrf_cookies": True,
+    'debug': True,
+    "ui_modules": ui,
+
 }
 
-        
-        
 application = tornado.web.Application([
-	(r"/", MainHandler),
-	(r"/e/.*", SMHandler),
-	(r"/debug(/.*|)$", DebugHandler),
-	(r"/dev(/.*|)$", DevHandler),
-	(r"/api(/.*|)$", api),
-	(r"/login(/.*|)$", LoginHandler), 
-	(r"/files/(.*)", tornado.web.StaticFileHandler, {"path": "static/files/"}),
+    (r"/", MainHandler),
+    (r"/e/.*", SMHandler),
+    (r"/debug(/.*|)$", DebugHandler),
+    (r"/dev(/.*|)$", DevHandler),
+    (r"/api(/.*|)$", Api),
+    (r"/login(/.*|)$", LoginHandler),
+    (r"/files/(.*)", tornado.web.StaticFileHandler, {"path": "static/files/"}),
 ], **settings)
 
 http_server = tornado.httpserver.HTTPServer(application)
-http_server.listen(port)#, address='0.0.0.0')
+http_server.listen(port)  # , address='0.0.0.0')
 print('started')
 tornado.ioloop.IOLoop.current().start()
