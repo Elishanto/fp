0e89266b661ae40ae3b758b06611ac7627c8772b
elishanto <elishanto@gmail.com>
2016-05-11 21:58:18 +0300

    Add localdata basedata tree copy

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
 import yaml
+import shutil
+
+if not os.path.exists('localdata'):
+    shutil.copytree('basedata/', 'localdata/')
 for var, value in yaml.load(open('localdata/config.yml', 'rb')).items():
     os.environ[var] = str(value)
 do = __import__('do')
