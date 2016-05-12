59b4a718ed1d7f101f111c503d5dd4e5434b3bc7
Dima Blaginin <dm.blaginin@gmail.com>
2016-05-12 21:45:45 +0500

    Added future tasks

--- handler/__init__.py
+++ handler/__init__.py
@@ -4,3 +4,4 @@ from .debug import DebugHandler
 from .login import LoginHandler
 from .main import MainHandler
 from .sm import SMHandler
+from .user import UserHandler
---
+++ handler/user.py
@@ -0,0 +1,21 @@
+available = ['register', 'baseinfo']
+
+class UserHandler(BaseHandler):
+	def get(self, url):
+		action = url.split('/')
+		now = action[0]
+
+		if now in available:
+			eval('self.'+now(action[1:])
+		else:
+			self.write('Задача неопознана')
+
+	def register(self, url):
+		referal = self.get_argument("ref", '', True)
+		self.render('../static/register_activity.html', referal=referal)
+	
+	def baseinfo(self, url):
+		""" 	Взять базовую инфу о пользователе через BaseHandler.get_user_info
+			... и вывести имя, фамилию, вес, программу и пр
+			И сделать доступным редактирование параметров (метод api SET — смотри JS функцию upload_sysinfo)
+			"""
