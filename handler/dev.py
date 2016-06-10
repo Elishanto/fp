cda37df214485d23391168c995b350e2e074b22b
Dima Blaginin <dm.blaginin@gmail.com>
2016-06-10 12:59:21 +0500

    AA fix

--- handler/dev.py
+++ handler/dev.py
@@ -24,7 +24,7 @@ class DevHandler(BaseHandler):
                                digestmod=sha1)
                 if hmac.compare_digest(sh1.hexdigest(), sh0):
                     print('OK, GITHUB CHECKED\nBRANCH "{}"'.format(os.environ['GIT_BRANCH']))
-                    subprocess.Popen(['bash ./localdata/updater.sh', os.environ['GIT_BRANCH']])
+                    subprocess.Popen(['./localdata/updater.sh', os.environ['GIT_BRANCH']])
                 else:
                     print('THIS IS NOT GITHUB!') 
             else:
