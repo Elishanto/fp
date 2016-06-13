aa7578ef776276075dd352deeba6ddc129278ef9
elishanto <elishanto@gmail.com>
2016-06-13 15:39:46 +0300

    Add successful registration processing Now captcha resets when form is incorrect

--- handler/registration.py
+++ handler/registration.py
@@ -2,12 +2,14 @@ import json
 import os
 from handler import BaseHandler
 from api.baseapi import Api
-description = os.environ['description']
 import re
 import recaptcha2
 
+description = os.environ['description']
+
 EMAIL_CHECK = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$'
 CAPTCHA_SECRET_KEY = '6LdadiITAAAAAI0o59pEnTCHGCn5Zh4bB17PJ-bt'
+
 
 class RegistrationHandler(BaseHandler):
     def get(self, url):
@@ -18,30 +20,27 @@ class RegistrationHandler(BaseHandler):
         if lake == 'global':
             allowed = ('name', 'email', 'wt', 'ht', 'password')
             task = json.loads(self.get_argument("aim").strip())
-            
-            if not ('captcha' not in task.keys() or\
-            recaptcha2.verify(CAPTCHA_SECRET_KEY, task['captcha'], self.request.remote_ip)['success']):
+
+            if not ('captcha' not in task.keys() or
+                        recaptcha2.verify(CAPTCHA_SECRET_KEY, task['captcha'], self.request.remote_ip)['success']):
                 self.write(Api.generate_request_return(-100))
                 self.finish()
-                return 
-
-
-            try:
-                task = {i:task[i] for i in allowed}
-            except KeyError:
-                self.write(Api.generate_request_return(-90))
-                self.finish()  
                 return
 
+            try:
+                task = {i: task[i] for i in allowed}
+            except KeyError:
+                self.write(Api.generate_request_return(-90))
+                self.finish()
+                return
 
-
-            if re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', task['email']) is None\
-             or (not task['wt'].isdigit())\
-             or (not task['ht'].isdigit())\
-             or len(task['password']) == 0: 
+            if re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', task['email']) is None \
+                    or (not task['wt'].isdigit()) \
+                    or (not task['ht'].isdigit()) \
+                    or len(task['password']) == 0:
                 self.write(Api.generate_request_return(-91))
                 self.finish()
-                return 
+                return
 
             task['opened_ex'] = (10,)
             task['valid'] = 1
